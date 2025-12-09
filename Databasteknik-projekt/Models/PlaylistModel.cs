74fd2d9a1e742a4c5f643da74824552839f7e68a
karolina utsi <id23kui@cs.umu.se>
2025-12-09 11:20:46 +0100

    hej

---
+++ Databasteknik-projekt/Models/PlaylistModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Databasteknik_projekt.Models
+{
+    public class PlaylistModel
+    {
+        public int PlaylistID { get; set; }
+        public string PlaylistName { get; set; }
+        public int UserID { get; set; }
+        public TimeSpan LengthOfPlaylist { get; set; }
+
+
+        //Konstruktor som tar alla attribut       
+        public PlaylistModel(int playlistID, string playlistName, int userID, TimeSpan lengthOfPlaylist)
+        {
+            PlaylistID = playlistID;
+            PlaylistName = playlistName;
+            UserID = userID;
+            LengthOfPlaylist = lengthOfPlaylist;
+        }
+    }
+}
---
+++ Databasteknik-projekt/Models/SongModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Databasteknik_projekt.Models
+{
+    public class SongModel
+    {
+        public int SongID { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public TimeSpan Length { get; set; }
+
+
+        //Konstruktor som tar alla attribut       
+        public SongModel(int songID, string title, string album, TimeSpan length)
+        {
+            SongID = songID;
+            Title = title;
+            Album = album;
+            Length = length;
+        }
+    }
+}
